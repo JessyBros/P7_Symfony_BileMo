099b46b7a9f5bc12f083c345e82a00b1c1e1e571
SiProdZz <j.bros@hotmail.fr>
2021-02-04 12:40:33 +0100

    add user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,10 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\Customer;
+use App\Entity\User;
+use App\Repository\CustomerRepository;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Exception\NotEncodableValueException;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class UserController extends AbstractController
 {
@@ -29,5 +36,29 @@ class UserController extends AbstractController
         }
 
         return $this->json($user, 200, [], ['groups' => 'show_users']);
+    }
+
+    /**
+     * @Route("/api/add_user", name="add_user", methods={"POST"})
+     */
+    public function addUser(Request $request,SerializerInterface $serializer, EntityManagerInterface $manager, CustomerRepository $customer)
+    {
+        $idCustomer = $customer->findOneById(1);
+        $jsonPost = $request->getContent();
+
+        try {
+            $post = $serializer->deserialize($jsonPost, User::class, 'json');
+            $post->setCustomer($idCustomer);
+            $manager->persist($post);
+            
+            $manager->flush();
+
+            return $this->json($post, 201, [], ['groups' => 'show_users']);
+        } catch (NotEncodableValueException $e) {
+            return $this->json([
+                "status" => 400,
+                "message" => $e->getMessage()
+            ], 400);
+        }
     }
 }
