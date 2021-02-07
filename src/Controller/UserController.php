db8efca56b9b5b574d254794b7510ed88158a83a
SiProdZz <j.bros@hotmail.fr>
2021-02-07 12:39:03 +0100

    use param converter

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,16 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Repository\CustomerRepository;
 use App\Repository\UserRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Serializer\Exception\NotEncodableValueException;
-use Symfony\Component\Serializer\SerializerInterface;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class UserController extends AbstractController
 {
@@ -25,46 +19,10 @@ class UserController extends AbstractController
     }
 
     /**
-     * @Route("/api/user/{id}", name="user", methods={"GET"})
+     * @Route("/api/users/{id<[0-9]+>}", name="user", methods={"GET"})
      */
-    public function showUser(int $id, UserRepository $userRepository)
+    public function showUser(User $user)
     {
-        $user = $userRepository->findOneById($id);
-
-        if ($user == null) {
-            throw $this->createNotFoundException("L'utilisateur " . $id ." n'a pas été trouvé !");
-        }
-
         return $this->json($user, Response::HTTP_OK, [], ['groups' => 'show_users']);
-    }
-
-    /**
-     * @Route("/api/add_user", name="add_user", methods={"POST"})
-     */
-    public function addUser(Request $request, SerializerInterface $serializer, EntityManagerInterface $manager, CustomerRepository $customer, ValidatorInterface $validator)
-    {
-        $idCustomer = $customer->findOneById(1);
-        $jsonPost = $request->getContent();
-
-        try {
-            $post = $serializer->deserialize($jsonPost, User::class, 'json');
-            $post->setCustomer($idCustomer);
-
-            $errors = $validator->validate($post);
-            if (count($errors) > 0) {
-                return $this->json($errors, 400);
-            }
-
-            $manager->persist($post);
-            
-            $manager->flush();
-
-            return $this->json($post, 201, [], ['groups' => 'show_users']);
-        } catch (NotEncodableValueException $e) {
-            return $this->json([
-                "status" => 400,
-                "message" => $e->getMessage()
-            ], 400);
-        }
     }
 }
