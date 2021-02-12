81039540c4ca6a9c37d674a41114e153d273a020
SiProdZz <j.bros@hotmail.fr>
2021-02-12 19:28:22 +0100

    install knpPaginator and paging list of phones

--- src/Controller/PhoneController.php
+++ src/Controller/PhoneController.php
@@ -5,12 +5,13 @@ namespace App\Controller;
 use App\Entity\Customer;
 use App\Entity\Phone;
 use App\Repository\PhoneRepository;
+use ContainerPuP5l9S\PaginatorInterface_82dac15;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\SecurityContext;
-
+use Knp\Component\Pager\PaginatorInterface;
 
 /**
  * @Route("/api")
@@ -20,9 +21,15 @@ class PhoneController extends AbstractController
     /**
      * @Route("/phones", name="phones", methods={"GET"})
      */
-    public function listPhones(PhoneRepository $phoneRepository, Request $request): Response
+    public function listPhones(PhoneRepository $phoneRepository, Request $request, PaginatorInterface $paginator): Response
     {
-        return $this->json($phoneRepository->findAll(), Response::HTTP_OK, [], ['groups' => 'list_phones']);
+        $phones = $paginator->paginate(
+            $phoneRepository->findAll(),
+            $request->query->getInt('page', 1),
+            10
+        );
+
+        return $this->json($phones, Response::HTTP_OK, [], ['groups' => 'list_phones']);
     }
 
     /**
