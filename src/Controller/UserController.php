a29e0e90f8acc4ded92095a164c25ffcc0ac4fca
SiProdZz <j.bros@hotmail.fr>
2021-02-15 16:28:08 +0100

    Use voter (isGarented) to controll acces Customer

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use App\Repository\CustomerRepository;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,6 +15,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Serializer\Exception\NotEncodableValueException;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
+
 
 /**
  * @Route("/api")
@@ -30,10 +32,11 @@ class UserController extends AbstractController
 
     /**
      * @Route("/users/{id<[0-9]+>}", name="user", methods={"GET"})
+     * @IsGranted("MANAGE", subject="user", statusCode=403, message="Vous n'avez pas l'autorisation pour consulter les détails de cet utilisateur")
      */
-    public function showUser(User $user, UserRepository $userRepository, UserInterface $customer)
+    public function showUser(User $user, UserRepository $userRepository)
     {
-        return $this->json($userRepository->findOneBy(["id" => $user,"customer" => $customer]), Response::HTTP_OK, [], ['groups' => 'show_users']);
+        return $this->json($userRepository->findOneBy(["id" => $user]), Response::HTTP_OK, [], ['groups' => 'show_users']);
     }
 
     /**
@@ -66,18 +69,10 @@ class UserController extends AbstractController
 
     /**
      * @Route("/users/{id}", name="delete_user", methods={"DELETE"})
+     * @IsGranted("MANAGE", subject="user", statusCode=403, message="Vous n'avez pas l'autorisation pour supprimer cet utilisateur")
      */
-    public function deleteUser(User $user, UserRepository $userRepository, UserInterface $customer, EntityManagerInterface $manager)
-    {     
-        $userFromCustomer = $userRepository->findOneBy(['id' => $user, 'customer' => $customer]);
-
-        if (!$userFromCustomer) {
-            return $this->json([
-                "status" => Response::HTTP_BAD_REQUEST,
-                "message" => "Erreur lors de la suppresion de l'utilisateur"
-            ], Response::HTTP_BAD_REQUEST);
-        }
-
+    public function deleteUser(User $user, EntityManagerInterface $manager)
+    { 
         $manager->remove($user);
         $manager->flush();
         return $this->json(null,Response::HTTP_NO_CONTENT);
